835191de5ea58b516a859640059fdd7f61a65e3e
Iv3tt3 <reach.iv3tt3@gmail.com>
2023-12-09 21:05:11 +0000

    Initial commit - EJ 1 & 2

---
+++ 1_Ejercicio1/ejercicio1.js
@@ -0,0 +1,10 @@
+let usuario = { 
+    Nombre : "Alba",
+    Apellidos : 'Ruiz Torres',
+    Bootcamps : {
+        Nodejs:'2023-12-01',
+        Git: '2023-11-03',
+        React: '2024-02-01'
+    },
+    BusquedaActiva : true,
+};
