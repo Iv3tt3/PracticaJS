6931d5bd2b0d284ef7e4a60e506525b76c920ed4
Iv3tt3 <reach.iv3tt3@gmail.com>
2023-12-19 18:31:23 +0000

    Ej 5 calcular promedio catalogo

--- 5_Ejercicio5/catalogo_musical.js
+++ 5_Ejercicio5/catalogo_musical.js
@@ -1,5 +1,3 @@
-// Creamos el catalogo
-
 function crearCatalogo() {
 
     let canciones = []
@@ -24,29 +22,49 @@ function crearCatalogo() {
         }
     
     const buscarPorGenero = (genero) => {
-        resultado = canciones.filter(cancion => cancion.genero === genero)
+        let resultado = canciones.filter(cancion => cancion.genero === genero)
         let listadoPorGenero = listarCanciones(resultado)
         return listadoPorGenero
+    }
+
+    const calcularPormedioDuracion = () => {
+        let duracionTotal = canciones.reduce((acum, {nombre, genero, duracion}) => {
+                const hhmmss = duracion.split(':')
+                const horas = parseInt(hhmmss[0],10)
+                const min = parseInt(hhmmss[1],10)
+                const sec = parseFloat(hhmmss[2],10)
+
+                const segundosTotales = horas*3600 + min*60 + sec
+
+                return acum + segundosTotales
+
+            },0)
+            promedio = duracionTotal/canciones.length
+            stringPromedio = (Math.floor(promedio/3600)).toString().padStart(2,'0') +':'+
+                             (Math.floor((promedio%3600)/60)).toString().padStart(2,'0') +':'+
+                             (promedio%60).toString().padStart(2,'0')
+        
+        
+        return stringPromedio
+        
     }
 
     return {
         agregarCancion,
         listarCanciones,
         buscarPorGenero,
-
-
+        calcularPormedioDuracion,
     }
 }
 
 let mi_catalogo = crearCatalogo();
-mi_catalogo.agregarCancion('cancion1', 'elgenero1', '20:00:00')
-mi_catalogo.agregarCancion('cancion2', 'elgenero2', '20:00:00')
+mi_catalogo.agregarCancion('cancion1', 'elgenero1', '01:30:00')
+mi_catalogo.agregarCancion('cancion2', 'elgenero2', '00:30:30')
 
 
 console.log(mi_catalogo.listarCanciones())
 console.log(mi_catalogo.buscarPorGenero('elgenero1'))
-
-    // Calcular promedio (con reduce)
+console.log(mi_catalogo.calcularPormedioDuracion())
 
     
 
